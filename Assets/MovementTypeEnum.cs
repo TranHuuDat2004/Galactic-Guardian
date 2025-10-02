6154f6a955faf3e47dd26e332d9f401961f5fd60
Trần Hữu Đạt <522h0081@student.tdtu.edu.vn>
2025-10-02 21:23:26 +0700

    remove

--- Assets/MovementTypeEnum.cs
+++
@@ -1,7 +0,0 @@
-public enum MovementType
-{
-    Vertical,       // Di chuyển thẳng xuống
-    Diagonal,       // Di chuyển chéo và tự xoay
-    Formation,       // Đứng yên và di chuyển theo đội hình cha
-    Roaming // << THÊM DÒNG NÀY VÀO
-}
